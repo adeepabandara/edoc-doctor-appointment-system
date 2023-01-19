027cfe7bb53ad9be445ef57f370a8c76453a97fd
adeepabandara <84633686+adeepabandara@users.noreply.github.com>
2023-01-19 14:39:40 +0530

    Update settings.php

--- doctor/settings.php
+++ doctor/settings.php
@@ -265,6 +265,7 @@
             ';
         }elseif($action=='view'){
             $sqlmain= "select * from doctor where docid='$id'";
+            while ($row = $db->fetch_array($sqlmain))
             $result= $database->query($sqlmain);
             $row=$result->fetch_assoc();
             $name=$row["docname"];
