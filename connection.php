59a7f80ff9a9985f036f79a7ef6e0e4f5e6661df
Dushanee Gamage <dushanee2000@gmail.com>
2023-01-27 12:17:27 +0530

    add config

--- connection.php
+++ connection.php
@@ -1,11 +1,8 @@
 <?php 
-   $host = "localhost";
-   $username = "root";
-   $password = "";
-   $dbname = "edoc";
-   
-   $database = new mysqli($host, $username, $password, $dbname);
-   if ($database->connect_error){
-       die("Connection failed:  ".$database->connect_error);
-   }
+    include('config.php');
+
+    $database = new mysqli($host, $username, $password, $dbname);
+    if ($database->connect_error){
+        die("Connection failed:  ".$database->connect_error);
+    }
 ?>
