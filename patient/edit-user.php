c1747ce57c1f889b226befda2c5c5cb2b1c09348
Dushanee Gamage <dushanee2000@gmail.com>
2023-01-27 12:00:38 +0530

    Update edit-user.php

--- patient/edit-user.php
+++ patient/edit-user.php
@@ -1,35 +1,27 @@
-
-    <?php
+<?php
     
-    
-
     //import database
     include("../connection.php");
 
-
-
     if($_POST){
-        //print_r($_POST);
-        $result= $database->query("select * from webuser");
-        $name=$_POST['name'];
-        $nic=$_POST['nic'];
-        $oldemail=$_POST["oldemail"];
-        $address=$_POST['address'];
-        $email=$_POST['email'];
-        $tele=$_POST['Tele'];
-        $password=$_POST['password'];
-        $cpassword=$_POST['cpassword'];
-        $id=$_POST['id00'];
-        
+        $name = mysqli_real_escape_string($database, $_POST['name']);
+        $nic = mysqli_real_escape_string($database, $_POST['nic']);
+        $oldemail = mysqli_real_escape_string($database, $_POST["oldemail"]);
+        $address = mysqli_real_escape_string($database, $_POST['address']);
+        $email = mysqli_real_escape_string($database, $_POST['email']);
+        $tele = mysqli_real_escape_string($database, $_POST['Tele']);
+        $password = mysqli_real_escape_string($database, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($database, $_POST['cpassword']);
+        $id = mysqli_real_escape_string($database, $_POST['id00']);
+
         if ($password==$cpassword){
             $error='3';
-
             $sqlmain= "select patient.pid from patient inner join webuser on patient.pemail=webuser.email where webuser.email=?;";
             $stmt = $database->prepare($sqlmain);
             $stmt->bind_param("s",$email);
             $stmt->execute();
             $result = $stmt->get_result();
-            //$resultqq= $database->query("select * from doctor where docid='$id';");
+       
             if($result->num_rows==1){
                 $id2=$result->fetch_assoc()["pid"];
             }else{
@@ -39,19 +31,17 @@
 
             if($id2!=$id){
                 $error='1';
-                //$resultqq1= $database->query("select * from doctor where docemail='$email';");
-                //$did= $resultqq1->fetch_assoc()["docid"];
-                //if($resultqq1->num_rows==1){
-                    
+               
             }else{
 
-                //$sql1="insert into doctor(docemail,docname,docpassword,docnic,doctel,specialties) values('$email','$name','$password','$nic','$tele',$spec);";
-                $sql1="update patient set pemail='$email',pname='$name',ppassword='$password',pnic='$nic',ptel='$tele',paddress='$address' where pid=$id ;";
-                $database->query($sql1);
-                echo $sql1;
-                $sql1="update webuser set email='$email' where email='$oldemail' ;";
-                $database->query($sql1);
-                echo $sql1;
+
+                $sql1 = $database->prepare("update patient set pemail=?, pname=?, ppassword=?, pnic=?, ptel=?, paddress=? where pid=?");
+                $sql1->bind_param("ssssssi", $email, $name, $password, $nic, $tele, $address, $id);
+                $sql1->execute();
+
+                $sql1 = $database->prepare("update webuser set email=? where email=?");
+                $sql1->bind_param("ss", $email, $oldemail);
+                $sql1->execute();
                 
                 $error= '4';
                 
@@ -66,6 +56,7 @@
         
     }else{
         //header('location: signup.php');
+
         $error='3';
     }
     
