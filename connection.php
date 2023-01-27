62f18b71768657694e40112b33d5e682b69c6774
Dushanee Gamage <dushanee2000@gmail.com>
2023-01-27 12:13:45 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,11 @@
-<?php
-
-    $database= new mysqli("localhost","root","","edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
+<?php 
+   $host = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "edoc";
+   
+   $database = new mysqli($host, $username, $password, $dbname);
+   if ($database->connect_error){
+       die("Connection failed:  ".$database->connect_error);
+   }
 ?>
