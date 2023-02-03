c14f27ab59a8070d0a11c31d3474870cd090606b
lahiru1115 <lahirudissanayake15@gmail.com>
2023-02-03 12:16:52 +0530

    Lahiru Update 1

--- admin/add-new.php
+++ admin/add-new.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/animations.css">
+    <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/admin.css">
-        
+
     <title>Doctor</title>
     <style>
-        .popup{
+        .popup {
             animation: transitionIn-Y-bottom 0.5s;
         }
-</style>
+    </style>
 </head>
+
 <body>
     <?php
 
@@ -22,68 +24,68 @@
 
     session_start();
 
-    if(isset($_SESSION["user"])){
-        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
+    if (isset($_SESSION["user"])) {
+        if (($_SESSION["user"]) == "" or $_SESSION['usertype'] != 'a') {
             header("location: ../login.php");
         }
-
-    }else{
+    } else {
         header("location: ../login.php");
     }
-    
-    
+
+
 
     //import database
     include("../connection.php");
 
 
 
-    if($_POST){
+    if ($_POST) {
         //print_r($_POST);
-        $result= $database->query("select * from webuser");
-        $name=$_POST['name'];
-        $nic=$_POST['nic'];
-        $spec=$_POST['spec'];
-        $email=$_POST['email'];
-        $tele=$_POST['Tele'];
-        $password=$_POST['password'];
-        $cpassword=$_POST['cpassword'];
-        
-        if ($password==$cpassword){
-            $error='3';
-            $result= $database->query("select * from webuser where email='$email';");
-            if($result->num_rows==1){
-                $error='1';
-            }else{
+        $result = $database->query("select * from webuser");
+        $name = $_POST['name'];
+        $nic = $_POST['nic'];
+        $spec = $_POST['spec'];
+        $email = $_POST['email'];
+        $tele = $_POST['Tele'];
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
 
-                $sql1="insert into doctor(docemail,docname,docpassword,docnic,doctel,specialties) values('$email','$name','$password','$nic','$tele',$spec);";
-                $sql2="insert into webuser values('$email','d')";
+        if ($password == $cpassword) {
+            $error = '3';
+            // $result = $database->query("select * from webuser where email='$email';");
+
+            $stmt = $database->prepare("SELECT * FROM webuser WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows == 1) {
+                $error = '1';
+            } else {
+
+                $sql1 = "insert into doctor(docemail,docname,docpassword,docnic,doctel,specialties) values('$email','$name','$password','$nic','$tele',$spec);";
+                $sql2 = "insert into webuser values('$email','d')";
                 $database->query($sql1);
                 $database->query($sql2);
 
                 //echo $sql1;
                 //echo $sql2;
-                $error= '4';
-                
+                $error = '4';
             }
-            
-        }else{
-            $error='2';
+        } else {
+            $error = '2';
         }
-    
-    
-        
-        
-    }else{
+    } else {
         //header('location: signup.php');
-        $error='3';
+        $error = '3';
     }
-    
 
-    header("location: doctors.php?action=add&error=".$error);
+
+    header("location: doctors.php?action=add&error=" . $error);
     ?>
-    
-   
+
+
 
 </body>
+
 </html>
